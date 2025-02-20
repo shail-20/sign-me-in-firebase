e8d6cb781c3bcd9b2568948f2b44826b14d9fbb0
Shail Bala Tannu <112934745+shail-20@users.noreply.github.com>
2025-02-20 14:12:37 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -22,4 +22,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 // const analytics = getAnalytics(app);
-export { auth, provider };
+export { app, auth, provider };
